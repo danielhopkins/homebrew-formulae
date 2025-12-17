64920408c61016a3f0fb68ee49d48cdd1bfe975f
Dan Hopkins <dan@boulderhopkins.com>
2025-12-17 10:30:19 -0700

    Update reminders-cli to 2.6.1

--- Formula/reminders-cli.rb
+++ Formula/reminders-cli.rb
@@ -1,8 +1,8 @@
 class RemindersCli < Formula
   desc "Simple CLI for interacting with macOS Reminders"
   homepage "https://github.com/danielhopkins/reminders-cli"
-  url "https://github.com/danielhopkins/reminders-cli/releases/download/2.6.0/reminders.tar.gz"
-  sha256 "167093280ae9a760c6c697f4853c72783854f3e737b3097211dd32bbd124d1aa"
+  url "https://github.com/danielhopkins/reminders-cli/releases/download/2.6.1/reminders.tar.gz"
+  sha256 "b4895fd6a0cf2403ccfd51e5123128c1656f9db44034c19c441353b20aec205d"
   license "MIT"
 
   def install
