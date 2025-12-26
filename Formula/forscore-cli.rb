acf16385f4a1338d8892123d340d70ffc8b5ed15
Dan Hopkins <dan@boulderhopkins.com>
2025-12-26 11:45:13 -0700

    Add explicit version to forscore-cli formula

--- Formula/forscore-cli.rb
+++ Formula/forscore-cli.rb
@@ -2,6 +2,7 @@ class ForscoreCli < Formula
   desc "CLI tool for managing forScore metadata"
   homepage "https://github.com/danielhopkins/forscore-cli"
   url "https://github.com/danielhopkins/forscore-cli/releases/download/v2.1.0/forscore-macos-arm64.tar.gz"
+  version "2.1.0"
   sha256 "8450185b750ad1cd992b6d6c8cd3d04771ba6797cceb814bd78020ecb9bb0729"
   license "MIT"
 
