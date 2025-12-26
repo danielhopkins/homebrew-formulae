828d0e4cc4b2a76924fae11c47584977b042c3c4
Dan Hopkins <dan@boulderhopkins.com>
2025-12-26 14:31:39 -0700

    forscore-cli 2.2.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/forscore-cli.rb
+++ Formula/forscore-cli.rb
@@ -1,9 +1,9 @@
 class ForscoreCli < Formula
   desc "CLI tool for managing forScore metadata"
   homepage "https://github.com/danielhopkins/forscore-cli"
-  url "https://github.com/danielhopkins/forscore-cli/releases/download/v2.2.1/forscore-macos-arm64.tar.gz"
-  version "2.2.1"
-  sha256 "59e18eabe563e157a387f7a1f02f326469cc8d3c97e159766e1d1b114f72e2a7"
+  url "https://github.com/danielhopkins/forscore-cli/releases/download/v2.2.2/forscore-macos-arm64.tar.gz"
+  version "2.2.2"
+  sha256 "1a8725cec6d6ee3630f88cc985adb5ba39c7948fa1a40d4e248363e773df3388"
   license "MIT"
 
   def install
