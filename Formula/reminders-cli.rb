3785eb0941d2f1ec81900e46ff0b51e3adf335d4
Dan Hopkins <dan@boulderhopkins.com>
2026-05-04 10:33:53 -0400

    reminders-cli 2.7.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/reminders-cli.rb
+++ Formula/reminders-cli.rb
@@ -1,8 +1,8 @@
 class RemindersCli < Formula
   desc "Simple CLI for interacting with macOS Reminders"
   homepage "https://github.com/danielhopkins/reminders-cli"
-  url "https://github.com/danielhopkins/reminders-cli/releases/download/2.6.4/reminders.tar.gz"
-  sha256 "5b27f0eb06870f412cf0c72474e3e84cf694f2c813b93051cd19eec269b5e1e0"
+  url "https://github.com/danielhopkins/reminders-cli/releases/download/2.7.0/reminders.tar.gz"
+  sha256 "9720949393039b6025190e1f8700f17dc8f6c32a0a4888ff32a3e1ff8fb4b22c"
   license "MIT"
 
   def install
