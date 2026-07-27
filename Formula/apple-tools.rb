4cac71cb1f1f5ce1071cc4b86fa08b73e7e5924a
Dan Hopkins <dan@boulderhopkins.com>
2026-07-27 14:27:01 -0600

    apple-tools 26.727.8

--- Formula/apple-tools.rb
+++ Formula/apple-tools.rb
@@ -1,8 +1,8 @@
 class AppleTools < Formula
   desc "CLIs for local Apple app data: Notes, Mail, Reminders, Calendar, Contacts"
   homepage "https://github.com/danielhopkins/apple-tools"
-  url "https://github.com/danielhopkins/apple-tools/releases/download/v26.727.7/apple-tools-26.727.7.tar.gz"
-  sha256 "df44eea99897b8e87d9e6aefa33b4736c17ce3aff29edd443d7f1e68db8ed603"
+  url "https://github.com/danielhopkins/apple-tools/releases/download/v26.727.8/apple-tools-26.727.8.tar.gz"
+  sha256 "a3416b6d49a8c091ae6e92cebbd146e3126d7e54e2159c9fb24ebd093415f832"
   license "MIT"
 
   depends_on :macos
